b38d9ad7bc25c1e1ee0ee4e3e264b22f07c79167
Valeria-Flores <valeriaflores.pz@gmail.com>
2024-11-05 02:30:01 -0600

    railway

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ const ticketRepository = new TicketRepository();
 
 const nodemailer = require('nodemailer');
 
+// Inicializa el transporter con las variables de entorno
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 
 // Rutas para servir vistas
